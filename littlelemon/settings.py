a947a099c2973bbf59f4adfcddd9b978f9b6aaa2
GabrielCG <gabrielbolso19@gmail.com>
2023-10-25 16:22:36 -0300

    Set permissions based in token authentication

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'reservation',
+    'LittleLemonAPI',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +145,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+
+}
+
+DJOSER = {
+    "USER_ID_FIELD":"username"
+    }
